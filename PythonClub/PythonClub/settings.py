d2e15adf84f7e08b9974436afab7bba52bfd2ba4
Michael M. Hansen <Michael.M.Hansen@seattlecolleges.edu>
2020-04-23 21:56:12 -0700

    Database configuration.

--- PythonClub/PythonClub/settings.py
+++ PythonClub/PythonClub/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'PythonClub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pythonclubdb',
+        'USER': 'postgres',
+        'PASSWORD': 'P@ssw0rd1',
+        'HOST': 'localhost',
+        'PORT':'',
     }
 }
 
